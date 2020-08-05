b0752b4649c09e6dde4ae3a5da14111802923dfd
My-Value <67990679+My-Value@users.noreply.github.com>
2020-08-05 09:19:45 +0800

    Add files via upload

---
+++ src/Cell.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+      <!-- {{n}} -->
+    <div class="cell" @click="Click">
+    <template v-if="a">{{text}}</template>
+    <template v-else ></template>
+  </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props:['n'],
+  data() {
+    return {
+      a: false,
+      text:''
+    };
+  },
+  methods:{
+      Click(){
+          if (this.text!=="") {
+                return;  
+          }
+          this.a=true;
+          this.text=this.n%2===0?"x":"o";
+          this.$emit('click',this.text)
+      }
+  }
+};
+</script>
+
+<style>
+.cell {
+    border: 1px solid black;
+    width: 100px;
+    height: 100px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 80px;
+    line-height: 100px;
+}
+</style>
+
